7d147859b64c232b4af18d86b5e747c6046ae388
knagu <45361985+knagu@users.noreply.github.com>
2020-05-19 14:36:32 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -15,9 +15,10 @@ locals {
   private_subnets        = ["10.0.1.0/24"]
   public_subnets         = ["10.0.101.0/24"]
   environment            = "dev"
+  cluster_name           = "xcluster"
   kops_state_bucket_name = "${local.environment}-kops-state-example"
   // Needs to be a FQDN
-  kubernetes_cluster_name = "myfirstcluster.k8s.local"
+  kubernetes_cluster_name = "${local.cluster_name}.k8s.local"
   ingress_ips             = ["10.0.0.100/32", "10.0.0.101/32", "54.69.253.153/32", "34.219.192.132/32"]
   vpc_name                = "${local.environment}-vpc"
 
